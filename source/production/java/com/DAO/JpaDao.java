2af80f558d1fb9173825cb0e3a78091aa72e3dbd
craigmarroquin <craigmarroquin@CSU.fullerton.edu>
2016-05-10 06:06:00 -0700

    JPA abstract class

---
+++ source/production/java/com/DAO/JpaDao.java
@@ -0,0 +1,14 @@
+package com.DAO;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface JpaDao<T, ID extends Serializable> {
+    T save(T entity);
+    T update(T entity);
+    void delete(T entity);
+    T findById(ID id);
+    List<T> findAll();
+    void flush();
+}
---
+++ source/production/java/com/DAO/JpaDaoImpl.java
@@ -0,0 +1,69 @@
+package com.DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class JpaDaoImpl<T, ID extends Serializable> implements JpaDao<T, ID> {
+    private Class<T> persistentClass;
+
+    private EntityManager entityManager;
+
+    public JpaDaoImpl(Class<T> persistentClass){
+        this.persistentClass = persistentClass;
+    }
+
+    public Class<T> getPersistentClass() {
+        return persistentClass;
+    }
+
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public T save(T entity) {
+        getEntityManager().persist(entity);
+        return entity;
+    }
+
+    @Override
+    public T update(T entity) {
+        T mergedEntity = getEntityManager().merge(entity);
+        return mergedEntity;
+    }
+
+    @Override
+    public void delete(T entity) {
+        entity = getEntityManager().merge(entity);
+        getEntityManager().remove(entity);
+    }
+
+
+    @Transactional
+    @Override
+    public T findById(ID id) {
+        T entity = (T) getEntityManager().find(getPersistentClass(), id);
+        return entity;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<T> findAll() {
+        return getEntityManager()
+                .createQuery("select x from " + getPersistentClass().getSimpleName() + " x")
+                .getResultList();
+    }
+
+    @Override
+    public void flush() {
+        getEntityManager().flush();
+    }
+}
